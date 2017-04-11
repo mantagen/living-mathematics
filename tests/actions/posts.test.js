3cf103a8c5106d52d8fa36211d40598bd38e3acc
mantagen <mathsgregory@gmail.com>
2017-04-11 19:24:09 +0100

    Adding tests for actions, and  adding test helper functions

---
+++ tests/actions/posts.test.js
@@ -0,0 +1,95 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import nock from 'nock'
+
+import * as actions from './../../src/actions/posts'
+import { fetchUrlify } from './../../src/api/endpoints.js'
+import {
+  generateWPPostObject,
+  generateFetchParams,
+} from './../helpers/posts-data.js'
+
+const middlewares = [ thunk ]
+const mockStore = configureMockStore(middlewares)
+
+describe('async actions', () => {
+  afterEach(() => {
+    nock.cleanAll()
+  })
+
+  it('creates RECEIVE_POSTS when fetching pasts has been done', () => {
+    const id = 1
+    const fetchParams = generateFetchParams({ id })
+    const postResponse = generateWPPostObject()
+    nock(fetchUrlify(fetchParams))
+      .get('')
+      .reply(200, [postResponse])
+
+    const transformedPost = actions.postMap(postResponse)
+    const expectedActions = [
+      { type: actions.REQUEST_POSTS,
+        fetchParams },
+      { type: actions.RECEIVE_POSTS,
+        fetchParams,
+        posts: { [id]: transformedPost } }
+    ]
+    const store = mockStore({ todos: [] })
+
+    return store.dispatch(actions.fetchPosts(fetchParams))
+      .then(() => { // return of async actions
+        expectedActions[1].receivedAt = store.getActions()[1].receivedAt
+        expect(store.getActions()).toEqual(expectedActions)
+      })
+  })
+})
+
+describe('select a post', () => {
+  it('should create an action to select a post', () => {
+    const id = 1
+    const expectedAction = {
+      type: actions.SELECT_POST,
+      id
+    }
+    expect(actions.selectPost(id)).toEqual(expectedAction)
+  })
+})
+
+describe('select a category', () => {
+  it('should create an action to select a category', () => {
+    const category = 'blog'
+    const expectedAction = {
+      type: actions.SELECT_CATEGORY,
+      category
+    }
+    expect(actions.selectCategory(category)).toEqual(expectedAction)
+  })
+})
+
+describe('request posts', () => {
+  it('should create an action to request post', () => {
+    const fetchParams = {}
+    const expectedAction = {
+      type: actions.REQUEST_POSTS,
+      fetchParams
+    }
+    expect(actions.requestPosts(fetchParams)).toEqual(expectedAction)
+  })
+})
+
+describe('receive posts', () => {
+  it('should create an action to receive posts', () => {
+    const fetchParams = {}
+    const json = []
+    const expectedAction = {
+      type: actions.RECEIVE_POSTS,
+      fetchParams,
+      posts: {}
+    }
+    const actualAction = actions.receivePosts(fetchParams, json)
+    expect(actualAction.type).toEqual(expectedAction.type)
+    expect(actualAction.posts).toEqual(expectedAction.posts)
+    expect(actualAction.fetchParams).toEqual(expectedAction.fetchParams)
+    expect(actualAction).toHaveProperty('receivedAt')
+    expect(Number.isInteger(actualAction.receivedAt)).toEqual(true)
+  })
+})
---
+++ tests/helpers/posts-data.js
@@ -0,0 +1,92 @@
+// @flow
+import * as types from './../../src/types/types.js'
+
+export const generateFetchParams = (data: Object): types.FetchParams => Object.assign({
+
+}, data)
+export const generateWPPostObject = (data: Object): types.WPPost => Object.assign({
+  id: 1,
+  date: "2017-04-05T14:45:43",
+  date_gmt: "2017-04-05T14:45:43",
+  guid: {
+    rendered: "http://livingmathematics.techniqueandquo.uk/wp/?p=1"
+  },
+  modified: "2017-04-06T18:13:51",
+  modified_gmt: "2017-04-06T18:13:51",
+  slug: "hello-world",
+  status: "publish",
+  type: "post",
+  link: "http://livingmathematics.techniqueandquo.uk/wp/2017/04/05/hello-world/",
+  title: {
+    rendered: "Extravaganza meetup"
+  },
+  content: {
+    rendered: "<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p> ",
+    protected: false
+  },
+  excerpt: {
+    rendered: "<p>Welcome to WordPress. This is your first post. Edit or delete it, then start writing!</p> ",
+    protected: false
+  },
+  author: 1,
+  featured_media: 0,
+  comment_status: "open",
+  ping_status: "open",
+  sticky: false,
+  template: "",
+  format: "standard",
+  meta: [ ],
+  categories: [
+    3
+  ],
+  tags: [ ],
+  _links: {
+    self: [
+      {
+        href: "http://livingmathematics.techniqueandquo.uk/wp/wp-json/wp/v2/posts/1"
+      }
+    ],
+    collection: [
+      {
+        href: "http://livingmathematics.techniqueandquo.uk/wp/wp-json/wp/v2/posts"
+      }
+    ],
+    about: [
+      {
+        href: "http://livingmathematics.techniqueandquo.uk/wp/wp-json/wp/v2/types/post"
+      }
+    ],
+    author: [
+      {
+        embeddable: true,
+        href: "http://livingmathematics.techniqueandquo.uk/wp/wp-json/wp/v2/users/1"
+      }
+    ],
+    replies: [
+      {
+        embeddable: true,
+        href: "http://livingmathematics.techniqueandquo.uk/wp/wp-json/wp/v2/comments?post=1"
+      }
+    ],
+    "version-history": [{
+      href: "http://livingmathematics.techniqueandquo.uk/wp/wp-json/wp/v2/posts/1/revisions"
+    }],
+    "wp:attachment": [{
+      href: "http://livingmathematics.techniqueandquo.uk/wp/wp-json/wp/v2/media?parent=1"
+    }],
+    "wp:term": [{
+    taxonomy: "category",
+    embeddable: true,
+    href: "http://livingmathematics.techniqueandquo.uk/wp/wp-json/wp/v2/categories?post=1"
+    }, {
+      taxonomy: "post_tag",
+      embeddable: true,
+      href: "http://livingmathematics.techniqueandquo.uk/wp/wp-json/wp/v2/tags?post=1"
+    }],
+    curies: [{
+      name: "wp",
+      href: "https://api.w.org/{rel}",
+      templated: true
+    }]
+  }
+}, data)
