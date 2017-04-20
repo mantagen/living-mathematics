9310a945ba5ea46c96ce272d698ec47958b0b3ef
mantagen <mathsgregory@gmail.com>
2017-04-21 00:36:04 +0100

    Adding hapi server Adding npm run commands for hapi server Changing npm run commands for linting to include jsx Adding nodemailer for email sending Adding redux-form Creating form react component utilising redux-form Creating contact react page which displays the form Separating react routes from file with ReactDOM.render, to allow for SSR Implenting SSR

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { IndexRoute, Router, Route, browserHistory } from 'react-router'
+import { Router, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 
 import configureStore from './store/configure-store.js'
+import Routes from './routes.js'
 
-import App from './app.jsx'
+const initialState = window.__INITIAL_STATE__
 
-import About from './routes/about.jsx'
-import Contact from './routes/contact.jsx'
-import Page from './routes/page.jsx'
-import Posts from './routes/posts.jsx'
-import Post from './routes/post.jsx'
-import NoMatch from './routes/no-match.jsx'
-
-const store = configureStore()
+const store = configureStore(initialState)
 const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render((
   <Provider store={store}>
     <Router history={history}>
-      <Route component={App}>
-        <Route path='/' slug={'home'} component={Page} />
-        <Route path='about' slug={'about'} component={Page} />
-        <Route path='contact' slug={'contact'} component={Page} />
-        <Route path=':postType' component={Posts} />
-        <Route path=':postType/:id' component={Post} />
-      </Route>
+      { Routes }
     </Router>
   </Provider>
   ),
