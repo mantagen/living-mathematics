72a693c21c6d097fcc5c4a7944338c9d9be2496b
mantagen <mathsgregory@gmail.com>
2017-04-21 00:36:04 +0100

    Adding hapi server Adding npm run commands for hapi server Changing npm run commands for linting to include jsx Adding nodemailer for email sending Adding redux-form Creating form react component utilising redux-form Creating contact react page which displays the form Separating react routes from file with ReactDOM.render, to allow for SSR Implenting SSR

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router'
 
 import FeaturedImage from './components/featured-image.js'
 import Logo from './containers/logo.jsx'
 import Nav from './containers/nav.jsx'
 
 class App extends React.Component {
-  render() {
+  render () {
     return (
       <div className='site-wrapper'>
         <header>
